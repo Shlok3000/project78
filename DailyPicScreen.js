a1f976e42d7e7e780dad04eeb06200d8d51ed82f
Shlok3000 <71341641+Shlok3000@users.noreply.github.com>
2021-07-17 15:10:19 -0400

    Add files via upload

---
+++ DailyPicScreen.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class DailyPicScreen extends React.Component {
+    render(){
+        return(
+            <View>
+                style={{
+                    flex:1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}
+                <Text>Daily Picture Screen</Text>
+            </View>
+        )
+    }
+}
